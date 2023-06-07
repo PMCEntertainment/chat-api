da530858a3ad33d164b970722b8cf3c997f14a74
Phương Mỹ Chi <developer@phuongmychi.vn>
2023-06-07 21:13:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 
+const express = require('express');
+ const app = express();
+ const server = require('http').Server(app);
+ const io = require('socket.io')(server);
+
+
+ io.on('connection', socket => {
+   console.log('A client connected.');
+
+
+   socket.on('message', message => {
+     console.log('Received message:', message);
+     io.emit('message', message);
+   });
+
+
+   socket.on('disconnect', () => {
+     console.log('A client disconnected.');
+   });
+ });
+
+ 
+ const PORT = 3006;
+ server.listen(PORT, () => {
+   console.log(`Server started on port ${PORT}`);
+ });
